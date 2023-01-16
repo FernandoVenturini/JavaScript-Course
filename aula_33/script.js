d741a6765b79bd17fdb8e2df4430a34201500e45
Fernando <fernandoventurini9@hotmail.com>
2023-01-16 20:47:50 +0000

    class 33

--- aula_33/script.js
+++ aula_33/script.js
@@ -1,2 +1,14 @@
 const divTodas = [...document.getElementsByTagName('div')];
 const cursoTodos = [...document.getElementsByClassName('curso')];
+const cursosC1 = [...document.getElementsByClassName('c1')];
+const cursosC2 = [...document.getElementsByClassName('c1')];
+const cursoEspecial = document.getElementById('c1');
+
+const query_divTodas = [...document.querySelectorAll('div[class]')];
+const query_cursosTodos = [...document.querySelectorAll('.curso')];
+const query_cursosC1 = [...document.querySelectorAll('.c1')];
+const query_cursosC2 = [...document.querySelectorAll('.c2')];
+const query_cursoEspecial = document.querySelector('#c1');
+console.log(query_divTodas);
+console.log(query_cursosC1);
+console.log(query_cursoEspecial);
