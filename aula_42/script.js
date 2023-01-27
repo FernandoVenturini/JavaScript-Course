f4f90134d4d97260a67dc541469c428b65b5c03b
Fernando <fernandoventurini9@hotmail.com>
2023-01-27 18:14:48 +0000

    class 42

---
+++ aula_42/script.js
@@ -0,0 +1,48 @@
+/*
+const filtroMaior18 = (valor) => {
+    if(valor >= 18) {
+        return valor;
+    }
+}
+const idades = [15, 21, 30, 17, 18, 44, 12, 50];
+const maior = idades.filter(filtroMaior18);
+
+console.log(idades);
+console.log(maior);
+
+/*
+const filtroMaior18 = (valor, indice, array) => {
+
+}
+*/
+
+/*************************************************************************************************/
+
+/*
+const filtrarMaiorIdade = (valor) => {
+    if(valor >= 18) {
+        return valor;
+    }
+}
+
+const idades = [15, 21, 30, 17, 18, 44, 12, 50];
+const maiorIdade = idades.filter((val, indice, array) => {
+    if(val >= 18) {
+        return val;
+    }
+})
+
+const menorIdade = idades.filter((val) => {
+    if(val < 18) {
+        return val;
+    }
+})
+
+console.log(idades);
+console.log(maiorIdade);
+console.log(menorIdade);
+*/
+
+/*************************************************************************************************/
+
+
