b29955cf5c253658e303e44e647d89891fab722d
fernandoventurini <fernandoventurini9@hotmail.com>
2023-02-22 02:33:52 +0000

    class 53

--- aula_52/script.js
+++ aula_52/script.js
@@ -3,11 +3,12 @@ const caixa = document.querySelector('#caixa');
 let cores = ['Azul', 'Vermelho', 'Verde', ['Claro', 'Escuro', 'Medio']];
 let cursos = ['HTML', 'CSS', 'JavaScript', cores];
 
+console.log(cursos);
 console.log(cursos[3][3][2]);
 
-cursos.push('React.js');
+cursos.push('React.js');// Acrescenta
 cursos.push('Python');
-cursos.pop();
+cursos.pop(); // Retira
 cursos.unshift('Python');
 cursos.shift();
 
