6025aa361457fab335872b5e53751b4aea696f2a
Fernando <fernandoventurini9@hotmail.com>
2023-03-02 01:13:51 +0000

    class 57

---
+++ aula_57/script.js
@@ -0,0 +1,14 @@
+class People {
+    constructor(pname) {
+        this.name = pname;
+    }
+}
+
+let p1 = new  People('Fernano');
+let p2 = new  People('Lavinia');
+let p3 = new  People('Viecnzo');
+let p4 = new  People('Fiorella');
+console.log(p1.name);
+console.log(p2.name);
+console.log(p3.name);
+console.log(p4.name);
