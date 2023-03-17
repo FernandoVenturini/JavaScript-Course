b0a9681faae50dafb768284391cb0f5ce4c4f2b1
Fernando <fernandoventurini9@hotmail.com>
2023-03-17 23:55:01 +0000

    carrion class 66

--- aula_66/script.js
+++ aula_66/script.js
@@ -18,6 +18,7 @@ class Npc {
 const npc1 = new Npc(100);
 const npc2 = new Npc(80);
 const npc3 = new Npc(30);
+const npc4 = new Npc(30);
 
 //console.log(npc1);
 //console.log(npc2);
@@ -27,3 +28,4 @@ Npc.alertar = true;
 npc1.info();
 npc2.info();
 npc3.info();
+npc4.info();
