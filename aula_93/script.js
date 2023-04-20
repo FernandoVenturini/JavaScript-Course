43336f14ebc3844589c3f758b6ed94ae8aa0ae42
fernandoventurini <fernandoventurini9@hotmail.com>
2023-04-21 00:48:42 +0100

    carrion class 93

--- aula_93/script.js
+++ aula_93/script.js
@@ -1,1 +1,6 @@
-alert('Aula 93!');
+//import {cursos, getTodosCursos} from 'script02.js';
+import getTodosCursos from 'script02.js';
+import { cursos } from './script02.js';
+
+console.log(cursos);
+console.log(getTodosCursos);
---
+++ aula_93/script02.js
@@ -0,0 +1,9 @@
+const cursos = ['JavaScript', 'Python', 'HTML', 'CSS', 'Reactjs', 'NodeJs'];
+
+const getTodosCursos = () => {
+    return cursos;
+}
+
+//export {cursos, getTodosCursos};
+export {cursos};
+export  default getTodosCursos;
