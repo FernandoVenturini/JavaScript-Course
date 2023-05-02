a21ecd534aa467629718a971313120e0f40788fd
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-02 20:45:49 +0100

    class 103

---
+++ aula_103/script.js
@@ -0,0 +1,10 @@
+const nome = new String('Fernando');
+
+let sobrenome = nome.slice(0, 4);
+console.log(sobrenome);
+
+let arr = nome.split('');
+console.log(arr);
+
+let parte1 = nome.substring(0, 5);
+console.log(parte1);
