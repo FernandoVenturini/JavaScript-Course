82a117c9b7cc105531c3b6e5be02dbe2680c0ded
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-05 00:16:56 +0100

    class 103 and 104

---
+++ aula_104/script.js
@@ -0,0 +1,12 @@
+let nome = new String('Fernando');
+console.log(nome.startsWith('F'));
+console.log(nome.endsWith('o'));
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
+console.log(nome.includes('i'));
+console.log(nome.charCodeAt(0));
+console.log(String.fromCodePoint(70));
+
+
+let nome_templateString = 'Fernando';
+console.log(`Nome: ${nome_templateString}`);
