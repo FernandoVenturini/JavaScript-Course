fec4b6822a4dadd26fe8e78c4fd3e8fdfdc6ae39
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-05 13:39:38 +0100

    class 105

---
+++ aula_105/script.js
@@ -0,0 +1,11 @@
+let nome = new String('Fernando Venturini');
+console.log(nome);
+
+console.log(nome.search('t'));
+console.log(nome.search(/Venturini/i));
+console.log(nome.match(/i/ig));
+console.log(nome.replace(/Fernando/i, 'Teste'));
+console.log(nome.replace(/i/ig, 'Teste'));
+
+
+
