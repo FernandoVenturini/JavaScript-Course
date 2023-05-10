3684f5f14dc3f7ed539374273c014b1faed3b9ca
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-10 23:35:34 +0100

    class 113

---
+++ aula_113/script.js
@@ -0,0 +1,23 @@
+const q1 = document.querySelector('#q1');
+const q2 = document.querySelector('#q2');
+const posx = document.querySelector('#posx');
+const posy = document.querySelector('#posy');
+const largura = document.querySelector('#largura');
+const altura = document.querySelector('#altura');
+
+q1.accesKey
+
+const info = (evt) => {
+    let DOMRect_q = `posx: ${DOMRect_q.x}`;
+    posy.innerHTML = `posy: ${DOMRect_q.y}`;
+    largura.innerHTML = `largura: ${DOMRect_q.width}`;
+    altura.innerHTML = `altura; ${DOMRect_q.height}`;
+}
+
+q1.addEventListener('click', (evt) => {
+    info(evt.target);
+})
+
+q2.addEventListener('click', (evt) => {
+    info(evt.target);
+})
