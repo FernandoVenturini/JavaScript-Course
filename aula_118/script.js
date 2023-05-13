508d8c3a3ffad5a79a6d91099dbd77686a68302e
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-13 11:35:29 +0100

    class 118

---
+++ aula_118/script.js
@@ -0,0 +1,26 @@
+let i = null; let j = 0;
+console.log(i, j);
+
+
+let canal = 'CFB Cursos';
+['a', 'b', 'c'].forEach(l => console.log(1));
+
+let n1 = [1, 2];
+let n2 = [3, 4];
+[n1, n2].map(v => console.log(v));
+
+let n = null;
+(n = 10);
+console.log(n);
+
+let numeros = [1, 2, 3, 4, 5];
+[n1, n2, n3] = numeros;
+console.log(n1, n2, n3);
+
+let num = {
+    n1: 10,
+    n2: 20,
+    n3: 30,
+}
+let {n1, n2, n3} = num;
+console.log(n1, n2, n3);
