ed3434d6cb869db96b6d4c212090a020e856acd3
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-28 19:38:17 +0100

    carrion class 22

--- aula_122/script.js
+++ aula_122/script.js
@@ -2,4 +2,10 @@ const p_texto = document.querySelector('#p_texto');
 const btn_texto = document.querySelector('#btn_texto');
 
 const endpoint = "https://cfbcursos.repl.co/";
-fetch()
+let res = fetch(endpoint)
+.then(res => {
+    res.json();
+})
+.then(datos => {
+    consoloe.log(datos);
+})
