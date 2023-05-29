500d337b35041a76abc037c80a419dd0c3f04c23
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-05-29 20:15:24 +0100

    class 123

---
+++ aula_123/script.js
@@ -0,0 +1,22 @@
+const p_texto = document.querySelector('#p_texto');
+const btn_texto = document.querySelector('#btn_texto');
+
+const p_temp = document.querySelector('#p_temp');
+const P_nivel = document.querySelector('#p_nivel');
+const p_press = document.querySelector('#p_press');
+
+const obterDados = () => {
+
+    const endpoint = "https://cfbcursos.repl.co/";
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(dados => {
+        console.log(dados);
+        p_temp.innerHTML = "Temperatura: " + dados.temperatura;
+        p_nivel.innerHTML = "Nivel: " + dados.nivel;
+        p_press.innerHTML = "Pressao: " + dados.pressao;
+    })
+}
+obterDados();
+
+let intervalo = setInterval(obterDados, 3000);
