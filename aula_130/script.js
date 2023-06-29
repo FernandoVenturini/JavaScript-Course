d471ef8cce4df2043dec75d3e07641967a1d471d
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-06-29 23:36:48 +0100

    class 130 - carrion

---
+++ aula_130/script.js
@@ -0,0 +1,10 @@
+const timer = document.querySelector('#timer');
+const tmpini = Date.now();
+const contador = () => {
+    const tmpatual =Date.now();
+    let cont = tmpatual - tmpini;
+    let seg = Math.floor((tmpatual - tmpini) / 1000);
+    //console.log(seg);    
+}
+setInterval(contador, 1000);
+
