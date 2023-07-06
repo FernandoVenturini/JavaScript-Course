4943b90128910f9226fcedfcd995dffbbd33765d
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-07-06 22:50:33 +0100

    end class 134

--- aula_134/topo.js
+++ aula_134/topo.js
@@ -1,21 +1,27 @@
 const body = document.body;
 
-head.innerHTML += estilo;
+const estiloTopo = 
+    "display: flex;"+
+    "justify-content: space-between;"+
+    "align-items: center;"+
+    "background-color: #00f;"
+
 
 const topo = document.createElement('div');
 topo.setAttribute("id", "topo");
-topo.setAttribute("class", "topo");
+topo.setAttribute("style", "estiloTopo");
 body.prepend(topo);
 
+const estilo_img_topo = "width: 200px"
 const logo = 
     "<div id='logo' class='logo'>" +
-        "<img src='logo.jpg' title='CFBCursos'/>" +
+        "<img src='logo.jpg' title='CFBCursos' style='"+ estilo_img_topo +"'/>" +
     "</div>"
-topo.innerHTML = logo;
+topo.innerHTML += logo;
 
 const login = 
     "<div id='login' class='login'>" +
         "<p id='matricula'>Matricula:<span></span></p>" +
         "<p id='nome'>Nome:<span></span></p>" +
     "</div>"
-topo.innerHTML += login;
+topo.innerHTML += login;  
