64a519913f3116652a3c650e11bb5a9d16ef49f9
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-07-22 20:56:14 +0100

    class 141, 142

---
+++ aula_142/login.js
@@ -0,0 +1,27 @@
+class Login {
+static logado = false;
+static matlogado = null;
+static nomelogado = null;
+static acessologado = null;
+static endpoint = "https://loginv1.cfbcursos.repl.co/";
+
+    static login = (mat, pas) => {
+        this.endpoint += `?matricula = ${mat} & senha = ${pas}`;
+        fetch(this.endpoint)
+        .then(response => response.json())
+        .then(response => {
+            if(response) {
+                this.logado = true;
+                this.matlogado = mat;
+                this.nomelogado = response.nome;
+                this.acessologado = response.acesso;
+                console.log(response);
+            }else{
+                console.log("Usuario nao encontrado!");
+            }
+        })
+        
+    }
+}
+
+export {Login};
