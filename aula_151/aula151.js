28dfc4cf03a8281608d1abe037471a60b1487c36
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-09-27 22:28:08 +0100

    class 151

---
+++ aula_151/aula151.js
@@ -0,0 +1,32 @@
+//import { Login } from "./login_cfbcursos.js";
+import { Cxmsg } from "../aula_138/cxmsg.js";
+
+const callback_ok = () => {
+    //alert("Ok!");
+    const config = {
+        color: "#009f00",
+        tipo: "ok",
+        textos: null,
+        comando_sn: null,
+    }
+    Cxmsg.show(config, "OK!","Login efetuado com sucesso!");
+}
+
+function callback_naook(){
+    //alert("Nao ok!");
+    const config = {
+        color: "#800",
+        tipo: "ok",
+        textos: null,
+        comando_sn: null,
+    }
+    Cxmsg.show(config, "ERROR!", "Login nao efetuado! Usuario ou senha nao encontrado!");
+}
+
+const configlogin = {
+    cor: "048",
+    img: "logo.png",
+    endpoint: "https://loginv1.cfbcursos.repl.co"
+}
+
+Login.login(callback_ok, callback_naook, configlogin);
