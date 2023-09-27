822eaeae769da32d0b058ba7dc61581e65ec137e
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-09-27 16:59:28 +0100

    class 149

---
+++ aula_150/script.js
@@ -0,0 +1,26 @@
+import { Login } from "./login.js";
+import { Cxmsg } from "../aula_138/cxmsg.js";
+
+const callback_ok = () => {
+    //alert("Ok!");
+    const config = {
+        color: "#0f0",
+        tipo: "ok",
+        textos: null,
+        comando_sn: null,
+    }
+    Cxmsg.show(config, "OK!","Login efetuado com sucesso!");
+}
+
+function callback_naook(){
+    //alert("Nao ok!");
+    const config = {
+        color: "#800",
+        tipo: "ok",
+        textos: null,
+        comando_sn: null,
+    }
+    Cxmsg.show(config, "ERROR!", "Login nao efetuado! Usuario ou senha nao encontrado!");
+}
+
+Login.login(callback_ok, callback_naook);
