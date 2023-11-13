78e29d8a102c42ee7b2779c8a0dafd59029d532a
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-11-13 21:21:24 +0000

    class 165

--- aula_165/agenda.js
+++ aula_165/agenda.js
@@ -8,19 +8,19 @@ const btn_sobre = document.querySelector("#btn_sobre");
 const principal = document.querySelector("#principal");
 
 btn_home.addEventListener('click', (evt) => {
-    abrirPagina(evt.target, "./home.html");
+    abrirPagina(evt.target, "./home.html","if_principal");
 });
 btn_novo.addEventListener('click', (evt) => {
-    abrirPagina(evt.target, "./novo.html");
+    abrirPagina(evt.target, "./novo.html","if_principal");
 });
 btn_pesquisar.addEventListener('click', (evt) => {
-    abrirPagina(evt.target, "./pesquisar.html");
+    abrirPagina(evt.target, "./pesquisar.html","if_principal");
 });
 btn_gestao.addEventListener('click', (evt) => {
-    abrirPagina(evt.target, "./gestao.html");
+    abrirPagina(evt.target, "./gestao.html","if_principal");
 });
 btn_sobre.addEventListener('click', (evt) => {
-    abrirPagina(evt.target, "./sobre.html");
+    abrirPagina(evt.target, "./sobre.html","if_principal");
 });
 
 const abrirPagina = (el, url) => {
