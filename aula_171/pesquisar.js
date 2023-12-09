e03c9770811586427c0a7ddb4f5bc0e941ebb3e0
Fernando Venturini <fernandoventurini9@hotmail.com>
2023-12-09 19:38:23 +0000

    class 172

--- aula_171/pesquisar.js
+++ aula_171/pesquisar.js
@@ -2,10 +2,18 @@ const btn_pesq = document.querySelector("#f_txtpesq");
 const f_txtpesq = document.querySelector("#f_txtpesq");
 
 btn_pesq.addEventListener('click', (evt) => {
-   if(f_txtpesq.value == "" ){
+   const valorpesq = f_txtpesq.value;
+   if(valorpesq == "" ){
       alert("Digite a pesquisa!");
       f_txtpesq.focus();
       return;
    }
-   console.log(document.getElementsByName("f_txtpesq").value);
+   const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
+   const endpoint = `http://127.0.0.1:1880/pesquisarcontatos/${f_txtpesq}/${valorpesq}`;
+   fetch(endpoint)
+   .then(res => res.json())
+   .then(res => {
+      console.log(res);
+   })
+   
 })
